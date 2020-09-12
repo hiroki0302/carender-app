ab3010b1ff6f5fd578cbfe480045610fdb92e91d
hiroki0302 <qishujiqi@gmail.com>
2020-09-12 16:05:53 +0900

    スケジュール編集・削除

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -20,21 +20,20 @@ class SchedulesController < ApplicationController
 
   # GET /schedules/1/edit
   def edit
+    @schedule = Schedule.find(params[:id])
   end
 
   # POST /schedules
   # POST /schedules.json
   def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      redirect_to root_path
+    else
+      @schedules = Schedule.all
+      render :index
+    end
 
-      
-        @schedule = Schedule.new(schedule_params)
-        if @schedule.save
-          redirect_to root_path
-        else
-          @schedules = Schedule.all
-          render :index
-        end
-    
   
 
     # respond_to do |format|
@@ -51,25 +50,36 @@ class SchedulesController < ApplicationController
   # PATCH/PUT /schedules/1
   # PATCH/PUT /schedules/1.json
   def update
-    respond_to do |format|
-      if @schedule.update(schedule_params)
-        format.html { redirect_to @schedule, notice: 'Schedule was successfully updated.' }
-        format.json { render :show, status: :ok, location: @schedule }
-      else
-        format.html { render :edit }
-        format.json { render json: @schedule.errors, status: :unprocessable_entity }
-      end
+    @schedule = Schedule.find(params[:id])
+    if @schedule.update(schedule_params)
+      redirect_to root_path, notice:"編集できました"
+    else
+      render 'edit'
     end
+  
+    # respond_to do |format|
+    #   if @schedule.update(schedule_params)
+    #     format.html { redirect_to @schedule, notice: 'Schedule was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @schedule }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @schedule.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /schedules/1
   # DELETE /schedules/1.json
   def destroy
+
+    @schedule = Schedule.find(params[:id])
     @schedule.destroy
-    respond_to do |format|
-      format.html { redirect_to schedules_url, notice: 'Schedule was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to root_path, notice:"削除しました"
+    # @schedule.destroy
+    # respond_to do |format|
+    #   format.html { redirect_to schedules_url, notice: 'Schedule was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
   end
 
   private
--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,74 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+const x = () => {
+
+  // const plan = document.getElementById("day-plan")
+  // console.log(schedule.plan);
+
+  // function () {
+  //   plan.textContent = plan ;
+  // };
+  const selectSchedules = document.querySelectorAll(".select-schedule")
+
+  let locked = false
+  num = 1
+  
+
+  selectSchedules.forEach(function (elem) {
+    let scheduleId = document.getElementById(`schedule-${elem.id}`);
+
+    // カーソルが乗ったら表示
+    elem.addEventListener('mouseover', function(){
+      if(!locked){
+        scheduleId.classList.remove("hide");
+      }
+    });
+
+    // クリックしたらビューに表示
+    elem.addEventListener('click', function(){
+      
+      console.log(num)
+      if(num % 2 !== 0){
+        locked = !locked
+        scheduleId.classList.remove("hide");
+        num += 1
+      }else{
+        locked = !locked
+        scheduleId.classList.add("hide");
+        num = 1
+      }
+      return
+      
+      // if(locked){
+      //   scheduleId.classList.add("hide");
+      // }else{
+      //   scheduleId.classList.remove("hide");
+      // }
+      // locked = !locked
+      // if(!locked){
+      //   scheduleId.classList.add("hide");
+      // }else{
+      //   scheduleId.classList.remove("hide");
+      // }
+      // locked = !locked
+
+
+    });
+
+
+    // カーソルが離れたら削除
+    elem.addEventListener('mouseout', function(){
+      if(!locked){
+        scheduleId.classList.add("hide");
+      }
+    });
+
+  });
+
+  
+};
+
+window.addEventListener("turbolinks:load", x);
+
